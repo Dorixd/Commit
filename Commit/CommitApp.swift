b2d64411c5690b4dd41cde8b7db107ba957a47e8
xdeng <dengxian0714@gmail.com>
2023-06-09 17:09:25 +0800

    Initial Commit

---
+++ Commit/CommitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitApp.swift
+//  Commit
+//
+//  Created by Nreal on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct CommitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Commit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Commit
+//
+//  Created by Nreal on 2023/6/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
